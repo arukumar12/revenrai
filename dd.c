0bc5e3776e7e8d439e5e9557193689552196094c
arukumar12 <30829827+arukumar12@users.noreply.github.com>
2018-01-09 16:43:32 +0530

    Create dd.c

---
+++ dd.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+{
+    long long m;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &m);
+
+    while(n != 0)
+    {
+      
+        n /= 10;
+        ++count;
+    }
+
+    printf("Number of digits %d", count);
+}
+	return 0;
+}
